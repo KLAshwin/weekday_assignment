cf1a4401c1b8142795cf483333372515c4cb464f
unknown <luckyashwink9@gmail.com>
2024-05-06 20:34:40 +0530

    experience filter synced

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ function App() {
     )
       .then((response) => response.json())
       .then((result) => {
+        console.log(jobData);
         setJobData((prevData) => [...prevData, ...result.jdList]);
         setLoading(false);
         setPage((prevPage) => prevPage + 1);
@@ -70,29 +71,55 @@ function App() {
         {jobData
           .filter((item) => {
             let searchCheck = false;
+            let expCheck = false;
+            let baseCheck = false;
+
             const roleFilterPassed =
               filterData.allRoles.length === 0 ||
               filterData.allRoles.includes(item.jobRole);
 
             for (let i = 0; i < filterData.allCompany.length; i++) {
-              console.log(filterData.allCompany[i], item.companyName.toLowerCase(), filterData.allCompany[i].startsWith(
-                item.companyName.toLowerCase()
-              ));
               if (
-                item.companyName.toLowerCase().startsWith(
-                  filterData.allCompany[i]
-                )
+                item.companyName
+                  .toLowerCase()
+                  .startsWith(filterData.allCompany[i])
               ) {
                 searchCheck = true;
                 break;
               }
             }
 
-            const companyFilterPassed =
-              filterData.allCompany.length === 0 ||
-              searchCheck;
+            for (let i = 0; i < filterData.allExp.length; i++) {
+              if (item.minExp > filterData.allExp[i]) {
+                expCheck = true;
+                break;
+              }
+            }
 
-            return roleFilterPassed && companyFilterPassed;
+            // for (let i = 0; i < filterData.allMinBase.length; i++) {
+            //   console.log(item.minJdSalary, filterData.allMinBase[i]?.substring(0, filterData.allMinBase[i]?.length - 1));
+            //   if (
+            //     item.minJdSalary?.substring(0, item.minJdSalary.length - 1) >
+            //     filterData.allMinBase[i]
+            //   ) {
+            //     baseCheck = true;
+            //     break;
+            //   }
+            // }
+
+            const companyFilterPassed =
+              filterData.allCompany.length === 0 || searchCheck;
+
+            const expFilterPassed = filterData.allExp.length === 0 || expCheck;
+
+            // const baseFilterPassed =
+            //   filterData.allMinBase.length === 0 || baseCheck;
+
+            return (
+              roleFilterPassed &&
+              companyFilterPassed &&
+              expFilterPassed
+            );
           })
           .map((item, idx) => (
             <JobCard item={item} idx={idx} />
